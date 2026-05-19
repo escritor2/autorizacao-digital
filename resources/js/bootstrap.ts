8e8e71163e465f1af931f639fbd0db28dffdad07
escritor2 <escritor759@gmail.com>
2026-05-19 16:15:54 -0300

    first commit

---
+++ resources/js/bootstrap.ts
@@ -0,0 +1,15 @@
+import axios, { AxiosStatic } from 'axios';
+
+declare global {
+    interface Window {
+        axios: AxiosStatic;
+    }
+}
+
+
+if (typeof window !== 'undefined') {
+    window.axios = axios;
+    window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+}
+
+export default axios;
